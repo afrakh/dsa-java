5738ddd3532387a3499cd0de9a6f3f789e3c061a
AfraKhurram <160423001+afrakh@users.noreply.github.com>
2025-08-07 21:12:59 +0500

    Add files via upload

---
+++ graphs/DFS.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+
+public class DFS {
+    public static class Edge{
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.weight = w;
+        }
+    
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph){
+        /*
+                    1 --------3       
+                  /           |  \     
+                0             |   5 ----6
+                  \           |  /         
+                    2 --------4  
+         
+         */
+
+         for (int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+         }
+
+         graph[0].add(new Edge(0, 1, 1));
+         graph[0].add(new Edge(0, 2, 1));
+
+         graph[1].add(new Edge(1, 0,1));
+         graph[1].add(new Edge(1, 3, 1));
+
+         graph[2].add(new Edge(2, 0, 1));
+         graph[2].add(new Edge(2, 4, 1));
+
+         graph[3].add(new Edge(3, 4, 1));
+         graph[3].add(new Edge(3, 1, 1));
+         graph[3].add(new Edge(3, 5, 1));
+
+         graph[4].add(new Edge(4, 2, 1));
+         graph[4].add(new Edge(4, 5, 1));
+         graph[4].add(new Edge(4, 5, 1));
+
+         graph[5].add(new Edge(5, 3, 1));
+         graph[5].add(new Edge(5, 4, 1));
+         graph[5].add(new Edge(5, 6, 1));
+
+         graph[6].add(new Edge(6, 5, 1));
+
+
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph, boolean visited[], int curr){
+        System.out.print(curr + " ");
+        visited[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+
+            if (!visited[e.dest]){
+                dfs(graph, visited, e.dest);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = (ArrayList<Edge>[]) new ArrayList[V]; //null -> empty ArrayList
+        createGraph(graph);
+
+        boolean[] visited = new boolean[V];
+        int curr = 0;
+        dfs(graph, visited, curr);
+    }
+}
